3b75fd9dcefd819256d996c3bc5d0ca23e5b5d3f
rr1980 <rener1980@googlemail.com>
2017-02-21 20:32:41 +0100

    reference fixed

--- src/CZWA.Web/wwwroot/_references.js
+++ src/CZWA.Web/wwwroot/_references.js
@@ -2,7 +2,7 @@
 /// <reference path="js/main.js" />
 /// <reference path="js/services/websocketservice.js" />
 /// <reference path="js/site.js" />
-/// <reference path="js/viewdatas/homeviewdata - kopieren.js" />
+/// <reference path="js/viewdatas/adminviewdata.js" />
 /// <reference path="js/viewdatas/homeviewdata.js" />
 /// <reference path="js/viewdatas/navbarviewdata.js" />
 /// <reference path="lib/bootstrap/dist/js/bootstrap.js" />
