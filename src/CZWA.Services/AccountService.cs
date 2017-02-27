6e4d5ca205b6eec7895ae2504f898259b1bf1e38
rr1980 <rener1980@googlemail.com>
2017-02-27 06:28:03 +0100

    fixed

--- src/CZWA.Services/AccountService.cs
+++ src/CZWA.Services/AccountService.cs
@@ -35,98 +35,25 @@ namespace CZWA.Services
         public async Task<bool> HasRole(UserRoleType urt)
         {
             var user = await GetCurrentUser();
-            if (user != null)
-            {
-                return  user.Roles.Any(r => r == (int)urt);
-            }
-            else
-            {
-                return false;
-            }
+            return _hasRole(user, urt);
         }
-
 
         public async Task<List<UserViewModel>> GetAllUsers()
         {
             var users = await _context.GetAllUsers();
-
-            var result = users.Select(user => new UserViewModel()
-            {
-                UserId = user.UserId,
-                Username = user.Username,
-                ShowName = user.Username,
-                Name = user.Name,
-                Vorname = user.Vorname,
-                Roles = _getRoles(user)
-            }).ToList();
-
-            return result;
+            return users.Select(user => _map(user)).ToList();
         }
 
         public async Task<UserViewModel> GetCurrentUser()
         {
             var id = Convert.ToInt32(_httpContext.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Sid).Value);
-            var user = await _context.GetUserById(id);
-
-            if (user != null)
-            {
-                return new UserViewModel()
-                {
-                    UserId = user.UserId,
-                    Username = user.Username,
-                    Name = user.Name,
-                    Vorname = user.Vorname,
-                    Roles = _getRoles(user)
-                };
-            }
-            else
-            {
-                return null;
-            }
-        }
-
-
-        private IEnumerable<int> _getRoles(User user)
-        {
-            var roles = user.RoleToUsers.Select(r => r.Role);
-            return roles.Select(r => (int)r.UserRoleType);
+            return await GetUserById(id);
         }
 
         public async Task<bool> Auth(string username, string password)
         {
-            var user = await _context.GetUser(username, password);
-
-            if (user != null)
-            {
-                var claims = new List<Claim> {
-                                 new Claim(ClaimTypes.Authentication, "true"),
-                                 new Claim(ClaimTypes.Sid, user.UserId.ToString()),
-                                 new Claim(ClaimTypes.Surname, user.Name),
-                                 new Claim(ClaimTypes.GivenName, user.Vorname),
-                                 new Claim(ClaimTypes.Name, user.Username)
-                        };
-
-                var uroles = user.RoleToUsers.Select(r => new Claim(ClaimTypes.Role, r.Role.UserRoleType.ToString()));
-                foreach (var role in uroles)
-                {
-                    claims.Add(role);
-                }
-
-
-                var claimsIdentity = new ClaimsIdentity(claims, "password");
-                var claimsPrinciple = new ClaimsPrincipal(claimsIdentity);
-
-
-                await _httpContext.Authentication.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrinciple, new AuthenticationProperties
-                {
-                    ExpiresUtc = DateTime.UtcNow.AddHours(12),
-                    IsPersistent = true,            // remember me!?
-                    AllowRefresh = true
-                });
-
-                return true;
-            }
-            return false;
+            var user = await _getUserByNameAndPw(username, password);
+            return await _auth(user);
         }
 
         public async Task<List<UserViewModel>> DelUser(UserViewModel user)
@@ -156,6 +83,101 @@ namespace CZWA.Services
             _context.SaveChanges();
 
             return await GetAllUsers();
+        }
+
+        public async Task<UserViewModel> GetUserById(int id)
+        {
+            var user = await _context.GetUserById(id);
+
+            if (user != null)
+            {
+                return _map(user);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        // ---------  privates
+
+        private async Task<UserViewModel> _getUserByNameAndPw(string username, string password)
+        {
+            var user = await _context.GetUser(username, password);
+            return _map(user);
+        }
+
+        private async Task<bool> _auth(UserViewModel user)
+        {
+            if (user != null)
+            {
+                var claims = new List<Claim> {
+                                 new Claim(ClaimTypes.Authentication, "true"),
+                                 new Claim(ClaimTypes.Sid, user.UserId.ToString()),
+                                 new Claim(ClaimTypes.Surname, user.Name),
+                                 new Claim(ClaimTypes.GivenName, user.Vorname),
+                                 new Claim(ClaimTypes.Name, user.Username)
+                        };
+
+                var uroles = user.Roles.Select(r => new Claim(ClaimTypes.Role, ((UserRoleType)r).ToString()));
+                foreach (var role in uroles)
+                {
+                    claims.Add(role);
+                }
+
+
+                var claimsIdentity = new ClaimsIdentity(claims, "password");
+                var claimsPrinciple = new ClaimsPrincipal(claimsIdentity);
+
+
+                await _httpContext.Authentication.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claimsPrinciple, new AuthenticationProperties
+                {
+                    ExpiresUtc = DateTime.UtcNow.AddHours(12),
+                    IsPersistent = true,            // remember me!?
+                    AllowRefresh = true
+                });
+
+                return true;
+            }
+            return false;
+        }
+
+        private UserViewModel _map(User user)
+        {
+            if (user != null)
+            {
+                return new UserViewModel()
+                {
+                    UserId = user.UserId,
+                    Username = user.Username,
+                    ShowName = user.Username,
+                    Name = user.Name,
+                    Vorname = user.Vorname,
+                    Roles = _getRoles(user)
+                };
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        private IEnumerable<int> _getRoles(User user)
+        {
+            var roles = user.RoleToUsers.Select(r => r.Role);
+            return roles.Select(r => (int)r.UserRoleType);
+        }
+
+        private bool _hasRole(UserViewModel user, UserRoleType urt)
+        {
+            if (user != null)
+            {
+                return user.Roles.Any(r => r == (int)urt);
+            }
+            else
+            {
+                return false;
+            }
         }
 
         private User _updateUser(User usr, UserViewModel user)
--- test/CZWA.Tests/Test_Services/Test_Service_Account.cs
+++ test/CZWA.Tests/Test_Services/Test_Service_Account.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Runtime.CompilerServices;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -10,7 +11,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection;
+using CZWA.ViewModels;
 
+[assembly: InternalsVisibleTo("Library.Tests")]
 namespace CZWA.Tests.Test_Services
 {
     [TestClass, Area("Rene")]
@@ -35,6 +39,59 @@ namespace CZWA.Tests.Test_Services
             var allUsers = await _accountService.GetAllUsers();
             Assert.IsNotNull(allUsers);
             Assert.IsTrue(allUsers.Count() >= 2);
+        }
+
+
+        [TestMethod]
+        [TestCategory("Smoke")]
+        public async Task GetUserById()
+        {
+            await Task.Run(async () =>
+            {
+                var user = await _accountService.GetUserById(1);
+
+                Assert.IsNotNull(user);
+                Assert.AreEqual("Riesner", user.Name);
+                Assert.AreEqual("rr1980", user.ShowName);
+                Assert.AreEqual(1, user.UserId);
+                Assert.AreEqual("rr1980", user.Username);
+                Assert.AreEqual("Rene", user.Vorname);
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Smoke")]
+        public async Task _hasRole()
+        {
+            await Task.Run(async () =>
+            {
+                var user = await _accountService.GetUserById(2);
+                Assert.IsNotNull(user);
+
+                var hasRole = (bool)typeof(AccountService).GetMethod("_hasRole", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_accountService, new object[] { user, UserRoleType.Default });
+                Assert.IsTrue(hasRole);
+                hasRole = (bool)typeof(AccountService).GetMethod("_hasRole", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_accountService, new object[] { user, UserRoleType.Admin });
+                Assert.IsFalse(hasRole);
+            });
+        }
+
+        [TestMethod]
+        [TestCategory("Smoke")]
+        public async Task _getUserByNameAndPw()
+        {
+            await Task.Run(async () =>
+            {
+                var user = await (Task<UserViewModel>)typeof(AccountService).GetMethod("_getUserByNameAndPw", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_accountService, new object[] { "rr1980", "123" });
+                Assert.IsNull(user);
+                user = await (Task<UserViewModel>)typeof(AccountService).GetMethod("_getUserByNameAndPw", BindingFlags.NonPublic | BindingFlags.Instance).Invoke(_accountService, new object[] { "rr1980", "12003" });
+                Assert.IsNotNull(user);
+                Assert.AreEqual("Riesner", user.Name);
+                Assert.AreEqual("rr1980", user.ShowName);
+                Assert.AreEqual(1, user.UserId);
+                Assert.AreEqual("rr1980", user.Username);
+                Assert.AreEqual("Rene", user.Vorname);
+
+            });
         }
     }
 }
